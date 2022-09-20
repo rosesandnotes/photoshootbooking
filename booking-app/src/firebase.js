a8205381254613422677dbab98467e9bdb4e42a5
Mary Rose Avenido <maryrose_avenido@yahoo.com>
2022-09-20 19:01:05 +0200

    added vuetify

---
+++ booking-app/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDPl8nAUyvuw3tIV0N2QxpuaHzXnKD-tRI",
+  authDomain: "booking-system-89684.firebaseapp.com",
+  projectId: "booking-system-89684",
+  storageBucket: "booking-system-89684.appspot.com",
+  messagingSenderId: "23148875378",
+  appId: "1:23148875378:web:71483763d562b54dda7ceb"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
